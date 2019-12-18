fd6f7588db3ba514f514450d5514d27f70c6d348
Alex <alexei.strelnikov@gmail.com>
2019-12-18 21:23:06 +0300

    замены String на Object Task1

--- src/main/java/Lesson1/Task1.java
+++ src/main/java/Lesson1/Task1.java
@@ -3,7 +3,7 @@ package Lesson1;
 (массив может быть любого ссылочного типа);*/
 public class Task1 {
     public static void main(String[] args) {
-        String[] arr = {"A", "B", "C", "D", "E", "F"};
+        Object[] arr = {"A", "B", "C", "ZZ", "E", "F"};
         System.out.printf("Исходный массив:");
         System.out.println();
         printArr(arr);
@@ -11,13 +11,13 @@ public class Task1 {
 
     }
 
-    private static void changePlaces(String[] arr, int firstElement, int secondElement) {
+    private static void changePlaces(Object[] arr, int firstElement, int secondElement) {
         if (firstElement < 0 || secondElement< 0){
             System.out.println("Индексы массива не могут быть меньше 0");
         }else if (firstElement > arr.length - 1 || secondElement > arr.length - 1) {
             System.out.println("Пытаешься поменять местами несуществующие элементы");
         }else {
-            String temp = arr[firstElement];
+            Object temp = arr[firstElement];
             arr[firstElement] = arr[secondElement];
             arr[secondElement] = temp;
             System.out.println("Измененный массив:");
@@ -25,7 +25,7 @@ public class Task1 {
         }
     }
 
-    private static void printArr(String[] arr) {
+    private static void printArr(Object[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
